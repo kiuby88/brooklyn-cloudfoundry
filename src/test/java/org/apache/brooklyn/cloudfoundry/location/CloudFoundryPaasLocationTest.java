0df5042875a5c9fa3c1a94571e182c9ca494f422
Kiuby88 <kiuby88.programming@gmail.com>
2016-06-23 14:30:09 +0200

    Adding a simple unit tests

--- src/test/java/org/apache/brooklyn/cloudfoundry/location/CloudFoundryPaasLocationTest.java
+++ src/test/java/org/apache/brooklyn/cloudfoundry/location/CloudFoundryPaasLocationTest.java
@@ -1,26 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.brooklyn.cloudfoundry.location;
+
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 import java.util.Map;
 
 import org.apache.brooklyn.core.test.BrooklynAppUnitTestSupport;
 import org.apache.brooklyn.util.collections.MutableMap;
-import org.cloudfoundry.client.lib.CloudFoundryClient;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class CloudFoundryPaasLocationTest extends BrooklynAppUnitTestSupport {
 
     protected CloudFoundryPaasLocation cloudFoundryPaasLocation;
 
     @Mock
-    protected CloudFoundryClient client;
+    CloudFoundryPaasApplicationFactory factory;
+
+    @Mock
+    CloudFoundryPaasApplication application;
 
     @BeforeMethod
     public void setUp() throws Exception {
         super.setUp();
         MockitoAnnotations.initMocks(this);
         cloudFoundryPaasLocation = createCloudFoundryPaasLocation();
+    }
+
+    @Test
+    public void testDeploy() {
+        when(factory.createApplication(cloudFoundryPaasLocation)).thenReturn(application);
+        cloudFoundryPaasLocation.setPaasApplicationFactory(factory);
+
+        CloudFoundryPaasApplication deployedApplication = cloudFoundryPaasLocation.deploy();
+        assertEquals(deployedApplication, application);
+
+        assertEquals(cloudFoundryPaasLocation.getDeployedApplications().size(), 1);
+        assertTrue(cloudFoundryPaasLocation.getDeployedApplications().contains(deployedApplication));
     }
 
     private CloudFoundryPaasLocation createCloudFoundryPaasLocation() {
